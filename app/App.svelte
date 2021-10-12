ae33b2e09474433c072b16647961651ba5c68ad8
Esteban Zapata Rojas <2417465+estebanz01@users.noreply.github.com>
2021-10-11 20:51:49 -0500

    feat: Initial commit.

---
+++ app/App.svelte
@@ -0,0 +1,21 @@
+<page>
+    <stackLayout>
+      <image src="~/images/icono-planta.jpg" height="150"/>
+      <label
+        class="info"
+        horizontalAlignment="center"
+        verticalAlignment="middle"
+        textWrap="true"
+        text="Iniciar sesi&oacute;n!"
+        height="80" />
+      <textField hint="Email" keyboardType="email" marginTop="3%" />
+      <textField hint="Password" secure="true" marginTop="3%" />
+      <button text="Ingresar" class="-primary -outline" marginTop="20%" />
+    </stackLayout>
+</page>
+
+<style>
+    .info {
+        font-size: 16;
+    }
+</style>
