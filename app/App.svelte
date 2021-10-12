bd4bc39b4770c896d182213187f36c6a563f4ac5
Esteban Zapata Rojas <2417465+estebanz01@users.noreply.github.com>
2021-10-11 21:26:09 -0500

    feat: Cambiar ícono de planta de inicio de sesión.

--- app/App.svelte
+++ app/App.svelte
@@ -1,13 +1,14 @@
 <page>
     <stackLayout>
-      <image src="~/images/icono-planta.jpg" height="150"/>
+      <image src="~/images/planta.png" height="150"/>
       <label
         class="info"
         horizontalAlignment="center"
         verticalAlignment="middle"
         textWrap="true"
-        text="Iniciar sesi&oacute;n!"
-        height="80" />
+        text="Iniciar sesi&oacute;n"
+        height="80"
+        marginTop="3%" />
       <textField hint="Email" keyboardType="email" marginTop="3%" />
       <textField hint="Password" secure="true" marginTop="3%" />
       <button text="Ingresar" class="-primary -outline" marginTop="20%" />
