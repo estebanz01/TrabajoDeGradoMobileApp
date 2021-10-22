da5c7db569a0942f98eb2f4adc0e66e134e0d1ca
Esteban Zapata Rojas <estebanz01@protonmail.com>
2021-10-21 22:47:29 -0500

    feat: Adicionar vistas para selección de cultivo y estado.

---
+++ app/Inicio.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { navigate } from 'svelte-native'
+  import ExplotacionAgricola from './ExplotacionAgricola.svelte'
+
+  const cultivoTransitorio = () => navigate({ page: ExplotacionAgricola });
+</script>
+<page>
+  <stackLayout>
+    <gridLayout columns="300, 300" rows="100">
+      <label
+        class="info"
+        text="AMCA"
+        horizontalAlignment="left"
+        textWrap="true"
+        fontSize="40em"
+        marginLeft="25%"
+        marginTop="3%" />
+      <image
+          src="~/images/planta.png"
+          height="80"
+          horizontalAlignment="right" />
+    </gridLayout>
+    <label
+      class="info"
+      horizontalAlignment="center"
+      verticalAlignment="middle"
+      textWrap="true"
+      text="Inicio"
+      fontSize="20em"
+      marginTop="10%" />
+    <button text="Estado del bien producido" class="-primary -outline" marginTop="20%" />
+    <button text="Estado de la naturaleza" class="-primary -outline" marginTop="3%" on:tap="{cultivoTransitorio}" />
+  </stackLayout>
+</page>
