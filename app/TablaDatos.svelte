77168a300f327222075cf30bfa324460ae2c65c3
Esteban Zapata Rojas <2417465+estebanz01@users.noreply.github.com>
2021-10-22 13:26:42 -0500

    feat: Añadir vista de tabla de datos transitorios.

---
+++ app/TablaDatos.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Template } from 'svelte-native/components';
+
+  const tablaVariablesDatos = require('~/tabla_datos.json');
+
+  let variables = tablaVariablesDatos.transitorios;
+</script>
+<page class="body">
+  <scrollView orientation="vertical">
+    <stackLayout>
+      <label
+        class="info"
+        text="Datos"
+        horizontalAlignment="center"
+        verticalAlignment="middle"
+        textWrap="true"
+        fontSize="40em"
+        marginTop="3%" />
+      <listView items="{variables}">
+        <Template let:item>
+          <label text="Ingrese el valor de {item.text}" textWrap="true" />
+          <textField keyboardType="number" hint="Valor" />
+        </Template>
+      </listView>
+      <button text="Continuar" class="-success -outline btn" />
+    </stackLayout>
+  </scrollView>
+</page>
