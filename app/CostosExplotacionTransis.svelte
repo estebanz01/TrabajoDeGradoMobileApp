1fc7054dc10eadfafb355bfc926a655bcaf66d05
Santiago Acevedo Gil <45128348+sacevedog@users.noreply.github.com>
2021-11-08 01:13:22 -0500

    Modificación de vista
    Se modifica la vista para visualizar los resultados.

--- app/CostosExplotacionTransis.svelte
+++ app/CostosExplotacionTransis.svelte
@@ -1,27 +1,34 @@
 <script>
-    import { navigate } from 'svelte-native'
-    import { alert } from '@nativescript/core/ui/dialogs';
-    import ExplotacionAgricola from './ExplotacionAgricola.svelte'
-  
-    const cultivoTransitorio = () => navigate({ page: ExplotacionAgricola });
-    document.write(uno);
+  import { navigate } from 'svelte-native'
+  import ExplotacionAgricola from './ExplotacionAgricola.svelte'
 
-    alert({ title: 'Variable', message: {uno}, okButtonText: 'OK' })
-  </script>
-  <page class="body">
-    <actionBar class="title" style="color: black;" title="Costos" />
-    <stackLayout>
-        
-      <gridLayout columns="300, 300" rows="100">
+  const cultivoTransitorio = () => navigate({ page: ExplotacionAgricola });
+</script>
+<page class="body">
+  <stackLayout>
+    <gridLayout columns="300, 300" rows="100">
       <label
         class="info"
-        horizontalAlignment="center"
-        verticalAlignment="middle"
+        text="AMCA"
+        horizontalAlignment="left"
         textWrap="true"
-        text="Inicio"
-        fontSize="20em"
-        marginTop="10%" />
-      <button text="Estado del bien producido" class="-success  btn" marginTop="20%" />
-      <button text="Estado de la naturaleza" class="-success  btn" marginTop="3%" on:tap="{cultivoTransitorio}" />
-    </stackLayout>
-  </page>
+        fontSize="40em"
+        marginLeft="25%"
+        marginTop="3%" />
+      <image
+          src="~/images/planta.png"
+          height="80"
+          horizontalAlignment="right" />
+    </gridLayout>
+    <label
+      class="info"
+      horizontalAlignment="center"
+      verticalAlignment="middle"
+      textWrap="true"
+      text="Inicio"
+      fontSize="20em"
+      marginTop="10%" />
+    <button text="Estado del bien producido" class="-success  btn" marginTop="20%" />
+    <button text="Estado de la naturaleza" class="-success  btn" marginTop="3%" on:tap="{cultivoTransitorio}" />
+  </stackLayout>
+</page>
