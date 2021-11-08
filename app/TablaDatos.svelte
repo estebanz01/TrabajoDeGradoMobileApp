817e578eac2378dc70b5a5595190641094efedef
Santiago Acevedo Gil <45128348+sacevedog@users.noreply.github.com>
2021-11-08 13:51:56 -0500

    Estilo al formulario

--- app/TablaDatos.svelte
+++ app/TablaDatos.svelte
@@ -89,7 +89,7 @@
         db.execSQL("CREATE TABLE IF NOT EXISTS c_transistorios (id INT(11), valor_kilogramo_inf VARCHAR(255), valor_kilogramo_sup VARCHAR(255), val_cg_var_unitario_inf VARCHAR(255), val_cg_var_unitario_sup VARCHAR(255), val_cg_fijo_unitario_inf VARCHAR(255), val_cg_fijo_unitario_sup VARCHAR(255), costo_total_prod_inf VARCHAR(255), costo_total_prod_sup VARCHAR(255), cg_var VARCHAR(255), cg_fijo VARCHAR(255), prod_limite_inf VARCHAR(255), prod_limite_sup VARCHAR(255))", function(err, table){
           console.log("Nueva tabla:", table);
         });
-        const ins = db.execSQL("INSERT INTO c_transistorios (valor_kilogramo_inf, valor_kilogramo_sup, val_cg_var_unitario_inf, val_cg_var_unitario_sup, val_cg_fijo_unitario_inf, val_cg_fijo_unitario_sup, costo_total_prod_inf, costo_total_prod_sup, cg_var, cg_fijo, prod_limite_inf, prod_limite_sup) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", ["20000","30000","40000","50000","60000","70000","80000","90000","22000","33000", "44000", "55000"], function(err, id) {
+        const ins = db.execSQL("INSERT INTO c_transistorios (valor_kilogramo_inf, valor_kilogramo_sup, val_cg_var_unitario_inf, val_cg_var_unitario_sup, val_cg_fijo_unitario_inf, val_cg_fijo_unitario_sup, costo_total_prod_inf, costo_total_prod_sup, cg_var, cg_fijo, prod_limite_inf, prod_limite_sup) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", ["20000","30000","40000","50000","60000","70000","80000","90000","22000","33000", "44000", "55000"], function(err, id) {
           console.log("The new record id is:", id);
         });
         db.all(
@@ -125,51 +125,51 @@
   <actionBar class="title" style="color: black;" title="Datos del cultivo" />
   <scrollView orientation="vertical" scrollBarIndicatorVisible="true">
     <stackLayout style="margin: 4px">
-      <label class="form-label mtop-16" height="30" width="240" text="Ingrese el valor de la producción en toneladas" textWrap="true" />
+      <label class="form-label mtop-16" text="Ingrese el valor de la producción en toneladas" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item1}"/>
-      <label class="form-label mtop-16" text="Ingrese el valor de la la devisación estándar de producción" textWrap="true" />
+      <label class="form-label mtop-16" text="Ingrese el valor de la la devisación estándar de producción" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item2}"/>
-      <label class="form-label" text="Ingrese la cantidad de jornales" textWrap="true" />
+      <label class="form-label" text="Ingrese la cantidad de jornales" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item3}"/>
-      <label class="form-label" text="Ingrese el valor de los jornales" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de los jornales" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item4}"/>
-      <label class="form-label" text="Ingrese el valor de los fertilizantes" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de los fertilizantes" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item5}"/>
-      <label class="form-label" text="Ingrese el valor de los insecticidas" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de los insecticidas" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item6}"/>
-      <label class="form-label" text="Ingrese el valor de otros insumos" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de otros insumos" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item7}"/>
-      <label class="form-label" text="Ingrese el valor de la asistencia técnica si es prestación por servicios" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de la asistencia técnica si es prestación por servicios" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item8}"/>
-      <label class="form-label" text="Ingrese el valor de asistencia técnica única vez" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de asistencia técnica única vez" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item9}"/>
-      <label class="form-label" text="Ingrese el valor del arrendamiento" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor del arrendamiento" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item10}"/>
-      <label class="form-label" text="Ingrese el valor de los servicios públicos" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de los servicios públicos" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item11}"/>
-      <label class="form-label" text="Ingrese el valor de los imprevistos" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de los imprevistos" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item12}"/>
-      <label class="form-label" text="Ingrese el valor de los impuestos" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de los impuestos" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item13}"/>
-      <label class="form-label" text="Ingrese el valor de las depreciaciones" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de las depreciaciones" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item14}"/>
-      <label class="form-label" text="Ingrese el valor del transporte" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor del transporte" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item15}"/>
-      <label class="form-label" text="Ingrese el valor de salarios de administración" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de salarios de administración" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item16}"/>
-      <label class="form-label" text="Ingrese el valor de mantenimientos preventivos" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de mantenimientos preventivos" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item17}"/>
-      <label class="form-label" text="Ingrese el valor de mantenimientos correctivos" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de mantenimientos correctivos" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item18}"/>
-      <label class="form-label" text="Ingrese la cantidad de semillas compradas" textWrap="true" />
+      <label class="form-label" text="Ingrese la cantidad de semillas compradas" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item19}"/>
-      <label class="form-label" text="Ingrese el valor de cada semilla" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor de cada semilla" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item20}"/>
-      <label class="form-label" text="Ingrese el valor del uso estimado de la tierra" textWrap="true" />
+      <label class="form-label" text="Ingrese el valor del uso estimado de la tierra" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item21}"/>
-      <label class="form-label" text="Prestaciones sociales" textWrap="true" />
+      <label class="form-label" text="Prestaciones sociales" textWrap="true" fontSize="16em" marginTop="10%" marginLeft="4%"/>
       <textField keyboardType="number" style="color: black;" hint="Valor" bind:text="{item22}"/>
-      <button text="Continuar" class="-success  btn" marginTop="20%" on:tap="{continuarResultado}" />
+      <button text="Continuar" class="-success  btn" marginTop="10%" on:tap="{continuarResultado}" />
       
     </stackLayout>
   </scrollView>
